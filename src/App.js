57ba107c52542f29172d1352f8fc1d99436936ac
SaeedhShareef <67280404+SaeedhShareef@users.noreply.github.com>
2024-07-08 01:11:08 -0400

    Updated css

--- src/App.js
+++ src/App.js
@@ -1,100 +1,110 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Chuck from "../src/Chuck_Norris_03.jpg";
-import bar from "../src/Generic+Cocktails+-+Christian+Horan.jpg";
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 
 import Button from 'react-bootstrap/Button';
-import { CardGroup } from "react-bootstrap";
 class App extends Component {
   state = {
     quote: "",
   };
 
-  getNewFact = () => {
+  getDrink = () => {
     axios
       .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then((res) => {
         console.log("response", res);
         let ingredientArray = [];
-        let clickedButton = false;
-        ingredientArray.push(
-          res.data.drinks[0].strIngredient1 +
-            "," +
-            res.data.drinks[0].strIngredient2 +
-            "," +
-            res.data.drinks[0].strIngredient3 +
-            "," +
-            res.data.drinks[0].strIngredient4 +
-            "," +
-            res.data.drinks[0].strIngredient5 +
-            "," +
-            res.data.drinks[0].strIngredient6 +
-            "," +
-            res.data.drinks[0].strIngredient7 +
-            "," +
-            res.data.drinks[0].strIngredient8 +
-            "," +
-            res.data.drinks[0].strIngredient9 +
-            "," +
-            res.data.drinks[0].strIngredient10 +
-            "," +
-            res.data.drinks[0].strIngredient11 +
-            "," +
-            res.data.drinks[0].strIngredient12 +
-            "," +
-            res.data.drinks[0].strIngredient13 +
-            "," +
-            res.data.drinks[0].strIngredient14 +
-            "," +
-            res.data.drinks[0].strIngredient15 
-        );
-        console.log(ingredientArray);
-        console.log(res.data.drinks[0].strDrink);
+        if(res.data.drinks[0].strIngredient1 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient1)
+        } 
+        if(res.data.drinks[0].strIngredient2 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient2)
+        } 
+        if(res.data.drinks[0].strIngredient3 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient3)
+        } 
+        if(res.data.drinks[0].strIngredient4 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient4)
+        } 
+        if(res.data.drinks[0].strIngredient5 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient5)
+        } 
+        if(res.data.drinks[0].strIngredient6 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient6)
+        } 
+        if(res.data.drinks[0].strIngredient7 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient7)
+        } 
+        if(res.data.drinks[0].strIngredient8 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient8)
+        } 
+        if(res.data.drinks[0].strIngredient9 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient9)
+        } 
+        if(res.data.drinks[0].strIngredient10 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient10)
+        } 
+        if(res.data.drinks[0].strIngredient11 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient11)
+        } 
+        if(res.data.drinks[0].strIngredient12 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient12)
+        } 
+        if(res.data.drinks[0].strIngredient13 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient13)
+        } 
+        if(res.data.drinks[0].strIngredient14 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient14)
+        } 
+        if(res.data.drinks[0].strIngredient15 != null){
+          ingredientArray.push(res.data.drinks[0].strIngredient15 )
+        } 
+
+        const filteredList = ingredientArray.map((items) => {
+          return <li> {items} </li>
+        })
+
         this.setState({
           drinkName: res.data.drinks[0].strDrink,
           drinkImage: res.data.drinks[0].strDrinkThumb + "/preview",
           drinkInstructions: res.data.drinks[0].strInstructions,
-          drinkIngredients: ingredientArray,
+          drinkIngredients: filteredList,
           clickedButton: true
         });
       });
+      console.log(this.state.clickedButton);
   };
   render() {
     return (
     
-      <div className="container left-align">
-        <h1 className="output">Random Cocktail Generator</h1>
-        <Button variant="primary" onClick={this.getNewFact}>Let's grab a drink</Button>
-
-        {/* <p>{this.state.drinkName}</p> */}
-        {/* <img src= alt='text' /> */}
-        {/* <span>{this.state.ingredientArray}</span>
-        <span>{this.state.drinkInstructions}</span> */}
-        {/* <h1>Chuck Norris Facts</h1>
-      <button onClick={ this.getNewFact}>CHUCK STUFF</button>
-      <div class="answerBox">
-      <p className="output">{this.state.quote}</p> */}
-        {/* </div> */}
+      <div className="container ">
         <div>
-          {this.state.clickedButton === true}
-          <span className="drinkCards">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, officia esse. Rem!</span>
-        <CardGroup  className="drinkCards">
-        <Card >
+        <h1 className="header">Random Cocktail Generator</h1>
+        <Button variant="primary" style={{ marginBottom: '3%', marginLeft: '43%' }} onClick={this.getDrink}>Let's grab a drink</Button>
+        </div>
+       
+        <div className="drinkCards">
+     
+        {this.state.clickedButton !== false}
+        <Row xs={1} md={1} lg={1} xl={3}>
+        <Card style={{ width: '16rem', marginBottom: '20px', marginLeft: 'auto', marginRight: 'auto' }}>
       <Card.Img variant="top" src={this.state.drinkImage} />
       <Card.Body>
-        <Card.Title className="cardTitle">{this.state.drinkName}</Card.Title>
+        <Card.Title style={{marginLeft: 'auto', marginRight: 'auto'}} >{this.state.drinkName}</Card.Title>
       </Card.Body>
     </Card>
-    <Card>
-      <Card.Title>Ingredients</Card.Title>
-      <Card.Body>{this.state.drinkIngredients}</Card.Body>
+    <Card style={{ width: '16rem',marginBottom: '20px', marginLeft: 'auto', marginRight: 'auto' }}>
+      <Card.Title style={{marginLeft: 'auto', marginRight: 'auto'}} >Ingredients</Card.Title>
+      <Card.Body>
+        {this.state.drinkIngredients}
+        </Card.Body>
     </Card>
-    <Card>
-      <Card.Title>Instructions</Card.Title>
+    <Card style={{ width: '16rem', marginBottom: '20px', marginLeft: 'auto', marginRight: 'auto' }}>
+      <Card.Title  style={{marginLeft: 'auto', marginRight: 'auto'}}>Instructions</Card.Title>
       <Card.Body>{this.state.drinkInstructions}</Card.Body>
     </Card>
-    </CardGroup>
+    </Row>
       </div>
       </div>
     );
