c9778e5972f9162accf2f035e0285ad6da244055
SaeedhShareef <67280404+SaeedhShareef@users.noreply.github.com>
2024-07-11 01:37:21 -0400

    Updating repository name

--- precache-manifest.9b53fe06023ab73fb27e356203fd7b4f.js
+++ precache-manifest.9b53fe06023ab73fb27e356203fd7b4f.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "dc557df45aa54dc35207dd050cc43613",
-    "url": "/chuckNorrisFacts/index.html"
+    "url": "/randomDrinkGenerator/index.html"
   },
   {
     "revision": "bfc3aacf359de5c812b8",
-    "url": "/chuckNorrisFacts/static/css/main.b96c67c3.chunk.css"
+    "url": "/randomDrinkGenerator/static/css/main.b96c67c3.chunk.css"
   },
   {
     "revision": "929c0d2d17c1494ce092",
-    "url": "/chuckNorrisFacts/static/js/2.98295ffa.chunk.js"
+    "url": "/randomDrinkGenerator/static/js/2.98295ffa.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/chuckNorrisFacts/static/js/2.98295ffa.chunk.js.LICENSE.txt"
+    "url": "/randomDrinkGenerator/static/js/2.98295ffa.chunk.js.LICENSE.txt"
   },
   {
     "revision": "bfc3aacf359de5c812b8",
-    "url": "/chuckNorrisFacts/static/js/main.9bc2b647.chunk.js"
+    "url": "/randomDrinkGenerator/static/js/main.9bc2b647.chunk.js"
   },
   {
     "revision": "84b7e79d64537b1f1d6e",
-    "url": "/chuckNorrisFacts/static/js/runtime-main.6c589804.js"
+    "url": "/randomDrinkGenerator/static/js/runtime-main.6c589804.js"
   },
   {
     "revision": "626e3a57a81bf077d03fb448189d08bd",
-    "url": "/chuckNorrisFacts/static/media/Chuck_Norris_03.626e3a57.jpg"
+    "url": "/randomDrinkGenerator/static/media/Chuck_Norris_03.626e3a57.jpg"
   }
 ]);
